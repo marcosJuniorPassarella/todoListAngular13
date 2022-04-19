023761b4a4b2105a0ced75ef297750c0f3daa63c
Marcosjr <mjworks2020@outlook.com>
2022-04-19 19:23:51 -0300

    Added form, methods and save in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from '../models/todo.model.ts';
 
 @Component({
@@ -6,16 +7,57 @@ import { Todo } from '../models/todo.model.ts';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public todos: Todo[] = [];
   public title: string = 'Minhas tarefas';
+  public form!: FormGroup;
 
-  constructor() {
-    this.todos.push(
-      new Todo(1, 'andar de carro', false),
-      new Todo(2, 'ir ao mercado', true),
-      new Todo(3, 'escrever código', false),
-      new Todo(4, 'reunião no trabalho', false)
-    );
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(50),
+          Validators.required,
+        ]),
+      ],
+    });
+    this.getTodosInLocalStorage();
+  }
+
+  addTodo() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.saveInLocalStorage();
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    // CAPTURA O INDICE DO TODO QUE RECEBEMOS POR PARÂMETRO
+    const index = this.todos.indexOf(todo);
+    // DELETA O ITEM DE NOSSA LISTA - ARRAY
+    index !== -1 && this.todos.splice(index, 1);
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+  saveInLocalStorage() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  getTodosInLocalStorage() {
+    const data = localStorage.getItem('todos') as string;
+    this.todos = JSON.parse(data);
   }
 }
