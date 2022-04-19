88a43d426e7a59eef3a1fbb1ef25ef3695f401b6
Marcosjr <mjworks2020@outlook.com>
2022-04-18 21:18:55 -0300

    Added model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit {
+  public todos: string[] = [];
+  public title: string = 'Minhas tarefas';
+
+  ngOnInit(): void {
+    this.todos.push(
+      'passear com o cachorro',
+      'andar a cavalo',
+      'comprar um polo'
+    );
+  }
 }
